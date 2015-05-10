ab922542faccad68a54f4987c5b23e0aba8aad2f
Ka-chanNi <wxtz521@gmail.com>
2015-05-11 02:26:51 +0800

    添加多层文件查找

---
+++ 0007/count_code.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import glob
+
+# get all files in designated path
+def get_files(path):
+	outputDir = []
+	finalDir = []
+	currentDir = glob.glob(path + r'\*')
+	for direct in currentDir:
+		if os.path.isfile(direct):
+			outputDir.append(direct)
+		elif os.path.isdir(direct):
+			nextDir = get_files(direct)
+			outputDir += nextDir
+	print(outputDir)
+	for i in range(len(outputDir)-1):
+		cutDir = outputDir[i].split('.')
+		print(cutDir)
+		if cutDir[-1] == 'py':
+			finalDir.append(outputDir[i])
+	print(finalDir)
+	return finalDir
+	
+# Count lines and blank lines and note lines in designated files
+def count_lines(files):
+	line, blank, note = 0, 0, 0
+	for filename in files:
+		f = open(filename, 'rb')
+		print(f)
+		for i in f:
+			i = i.strip()
+			i = i.decode()
+			line += 1
+			if i == '':
+				blank += 1
+			elif i[0] == '#' or i[0] == '/':
+				note += 1
+		f.close()
+	return (line, blank, note)
+	
+if __name__ == '__main__':
+	files = get_files('..')
+	print(files)
+	lines = count_lines(files)
+	print('Line(s): %d, blank line(s): %d, note line(s): %d'% (lines[0], lines[1], lines[2]))
