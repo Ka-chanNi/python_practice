485d3a359ae5e62a2303d3bab2a21d688f564592
Ka-chanNi <wxtz521@gmail.com>
2015-05-13 01:17:22 +0800

    第一版

---
+++ 0008/documents.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+
+def get_content(domain):
+	source = requests.get(domain)
+	text = source.text
+	html = BeautifulSoup(text, 'lxml')
+	content = html.find(id='link-report')
+	for string in content.strings:
+		print(repr(string) + '\n')
+
+
+
+if __name__ == '__main__':
+	domain = 'http://www.douban.com/note/384034502/'
+	get_content(domain)
