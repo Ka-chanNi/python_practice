363f790736fda3c174bb8d186b445a8bdf6d9474
Ka-chanNi <wxtz521@gmail.com>
2015-05-18 00:19:13 +0800

    第一版

---
+++ 0014/txt2xls.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import xlwt3
+import json
+
+def write_txt_to_xls(txt_file):
+	with open(txt_file, 'rb') as f:
+		file_content = f.read().decode('utf-8')
+		print(file_content)
+		file_json = json.loads(file_content)
+	
+	xls_object = xlwt3.Workbook()
+	sheet = xls_object.add_sheet('student')
+	for i in range(len(file_json)):
+		sheet.write(i, 0, i+1)
+		data = file_json[str(i+1)]
+		for j in range(len(data)):
+			sheet.write(i, j+1, data[j])
+	xls_object.save('student.xls')
+	
+	
+if __name__ == '__main__':
+	write_txt_to_xls('student.txt')
