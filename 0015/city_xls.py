f927bd6290a54855b1be0f6261db5b4690eeb466
Ka-chanNi <wxtz521@gmail.com>
2015-05-18 01:40:06 +0800

    第一版，合并了0014和0015

---
+++ 0015/city_xls.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import xlwt3
+import json
+
+def write_txt_to_xls(txt_file):
+	with open(txt_file, 'rb') as f:
+		file_content = f.read().decode('utf-8')
+		file_json = json.loads(file_content)
+		
+	xls_content = xlwt3.Workbook()
+	sheet = xls_content.add_sheet('city')
+	data_list = []
+	for i in range(len(file_json)):
+		sheet.write(i, 0, i+1)
+		data = file_json[str(i+1)]
+		if isinstance(data, list):
+			for j in range(len(data)):
+				sheet.write(i, j+1, data[j])
+		else:
+			sheet.write(i, 1, data)
+	xls_content.save('city.xls')
+	
+	
+if __name__ == '__main__':
+	write_txt_to_xls('city.txt')
